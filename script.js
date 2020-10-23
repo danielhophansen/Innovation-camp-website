f294edf8396cdd572573ed1722b4c6b86d1ecf5e
danielhophansen <70698766+danielhophansen@users.noreply.github.com>
2020-10-23 16:03:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', (e) => {
+    let array = Array.from(document.getElementsByClassName("button"));
+	array.map( (val) => val.addEventListener('click', e => changeModule.cmpWithArray(val)));
+	array[0].click();
+
+});	
+
+var changeModule = (function() {
+  'use strict';
+
+  let problems = ["Cost","Simplisity","Range","Implementation"];
+
+  return {
+    cmpWithArray: function(val) {
+		    let index     = problems.findIndex(x=>x===val.innerHTML);
+		    let solutions = Array.from(document.getElementsByClassName("solution"));
+	   
+			solutions.filter((val,idx)=> idx!=index).forEach(x=>x.style.display = "none");
+			solutions.filter((val,idx)=> idx==index).forEach(x=>x.style.display = "block");
+	        val.style.backgroundColor = "red";
+	        val.style.color = "white";
+			let array = Array.from(document.getElementsByClassName("button"));
+			array.filter((val,idx)=> idx!=index).forEach(x=>{
+				 x.style.backgroundColor = "black";
+	             x.style.color = "white";
+				
+			});
+    }	    
+  };
+})();
+
